770cd76878cc5be94e0344d4e3ce5c830a3b5b88
Praveena G <praveena.gunasekhar@gmail.com>
2012-06-12 18:34:26 +0530

    Added reek:instrument task for generating the html output

--- reek_viewer.rb
+++ reek_viewer.rb
@@ -1,15 +1,22 @@
 require 'rubygems'
 require 'erb'
-
 require_relative 'reek_parser'
 
-@input_file_path = ARGV.first
-reeks = ReeksList.new(@input_file_path)
-@reeks_list = reeks.divide_reeks
-@keys = reeks.context_class_list
-@sum_of_waning_num = reeks.total_warning_num
+class ReekViewer
+  def initialize(yaml_file)
+	@yaml_file = yaml_file
 
-erb_str = open(Dir::pwd + "/view_template.erb").read
-html = ERB.new(erb_str).result(binding)
+  end
 
-open("./" + @input_file_path + ".html", "w"){|f| f.write html }
+  def generate_html
+	reeks = ReeksList.new(@yaml_file)
+	@reeks_list = reeks.divide_reeks
+	@keys = reeks.context_class_list
+	@sum_of_waning_num = reeks.total_warning_num
+
+	erb_str = open(Dir::pwd + "/view_template.erb").read
+	html = ERB.new(erb_str).result(binding)
+
+	open("./" + @yaml_file + ".html", "w"){|f| f.write html }
+  end
+end
